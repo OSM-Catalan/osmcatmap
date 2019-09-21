d87386bf67788901f5b1adafa2efb78e11d1bd27
yopaseopor <yopaseopor@gmail.com>
2019-09-22 01:14:19 +0200

    Update layerdef.js

--- js_source/layerdef.js
+++ js_source/layerdef.js
@@ -376,14 +376,14 @@ function layerdef(type){
 			),
 
 make_layer(
-QURL + "?data=(relation['network'='exprés.cat (Barcelona)'](bbox););out+skel;",
+QURL + "?data=relation['network'='exprés.cat (Barcelona)'](bbox);out+skel;",
 name="#ex#&nbspExprés.cat Barcelona",
 defaultExtPoint("https://github.com/yopaseopor/beta_style_josm/raw/master/traffic_signs_EUR/ES/ES_R1.png"),
 false
 ),
 
 			make_layer(
-QURL + "?data=(relation['ref'='e18'](bbox););out+skel;",
+QURL + "?data=relation['ref'='e18'](bbox);out+skel;",
 				name="#l#Exprés.cat Barcelona",
 				defaultSolidLine("red"),
 				false,
